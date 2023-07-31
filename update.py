327c6805caf52760ceafc9b366303ffdb60a8439
wise0n <96984685+wise0n@users.noreply.github.com>
2023-07-30 21:27:01 -0400

    Code style update

--- update.py
+++ update.py
@@ -3,52 +3,52 @@ import sys
 import pathlib
 import requests
 
-def language_stats(url, token, readme_path):
+def language_stats(api_url, token, readme_path):
     headers = {
-            'Authorization': f'token {token}'
+        'Authorization': f'token {token}'
     }
 
-    request = requests.get(url, headers=headers)
+    request = requests.get(api_url, headers=headers, timeout=3)
     json = request.json()
 
-    python = 0
-    bash = 0
-    go = 0
-    c = 0
+    lang_python = 0
+    lang_bash = 0
+    lang_go = 0
+    lang_c = 0
 
     for language in json:
         lang_name = language['language']
         if lang_name == 'Python':
-            python += 1
+            lang_python += 1
         elif lang_name == 'Go':
-            go += 1
+            lang_go += 1
         elif lang_name == 'Shell':
-            bash += 1
+            lang_bash += 1
         elif lang_name == 'C':
-            c += 1
+            lang_c += 1
 
     updated_content = f'''
 ```python
                   _____       | | |
-_ _ _ _ ____ ____ |   | _  _  |_|_|           Python: {python}
-| | | | [__  |___ |   | |\ |    |             Bash: {bash}
-|_|_| | ___] |___ |___| | \|    |             Go: {go}
-                                |             C: {c}
+_ _ _ _ ____ ____ |   | _  _  |_|_|           Python: {lang_python}
+| | | | [__  |___ |   | |\ |    |             Bash: {lang_bash}
+|_|_| | ___] |___ |___| | \|    |             Go: {lang_go}
+                                |             C: {lang_c}
 
 ```
   '''
 
-    with open (readme_path, 'r') as readme:
+    with open (readme_path, 'r', encoding="utf-8") as readme:
         readme_content = readme.read()
 
     new_content = re.sub(r'(?<=<!\-\- start \-\->)[\s\S]*(?=<!\-\- end \-\->)', f'\n{updated_content}\n', readme_content)
 
-    with open (readme_path, 'w') as readme:
+    with open (readme_path, 'w', encoding="utf-8") as readme:
         readme.write(new_content)
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/user/repos'
-    token = sys.argv[1]
-    root_path = pathlib.Path(__file__).parent.resolve()
-    readme_path = f'{root_path}/README.md'
-    language_stats(url, token, readme_path)
+    API_URL = 'https://api.github.com/user/repos'
+    TOKEN = sys.argv[1]
+    ROOT_PATH = pathlib.Path(__file__).parent.resolve()
+    README_PATH = f'{ROOT_PATH}/README.md'
+    language_stats(API_URL, TOKEN, README_PATH)
