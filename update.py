819f1ef47c73dba6fece2548b257a68e34d65fd6
8manz <sw4xb7e9.jnjun@silomails.com>
2024-03-18 22:22:29 -0400

    style: changed to use Counter instead of count
    count would probably perform better than Counter
    since I'm only counting my repos, meaning the count
    is low and it's all strings.
    
    Generally insignificant, but looks better and is easier to type.

--- update.py
+++ update.py
@@ -2,6 +2,7 @@ import re
 import sys
 import pathlib
 from typing import NamedTuple
+from collections import Counter
 
 import requests
 
@@ -32,13 +33,13 @@ def language_stats(api_url, token, readme_path):
             'Haskell'
     ]
 
-    language_found_in_repos_list = [language['language'] for language in json if language['language'] in language_name_list]
+    language_found_in_repos_list = Counter([language['language'] for language in json if language['language'] in language_name_list])
 
     names = LanguageNames(
-            language_found_in_repos_list.count('Python'), language_found_in_repos_list.count('Shell'),
-            language_found_in_repos_list.count('Go'), language_found_in_repos_list.count('C'),
-            language_found_in_repos_list.count('C++'), language_found_in_repos_list.count('Julia'),
-            language_found_in_repos_list.count('Haskell')
+            language_found_in_repos_list['Python'], language_found_in_repos_list['Shell'],
+            language_found_in_repos_list['Go'], language_found_in_repos_list['C'],
+            language_found_in_repos_list['C++'], language_found_in_repos_list['Julia'],
+            language_found_in_repos_list['Haskell']
     )
 
     update_content(names, readme_path)
